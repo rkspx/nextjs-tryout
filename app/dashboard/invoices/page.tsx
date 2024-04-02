a31f7e7b3151b3ab0a43c526edc8068b35c5452a
rkspx <kosiriki@gmail.com>
2024-04-02 13:16:17 +0700

    chapter 10

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,21 @@
-export default function Page() {
-    return <p>Invoices page</p>
+import { lusitana } from "@/app/ui/fonts";
+import { CreateInvoice } from "@/app/ui/invoices/buttons";
+import Search from "@/app/ui/search";
+
+export default async function Page() {
+    return (
+        <div className="w-full">
+            <div className="flex w-full items-center justify-between">
+                <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+            </div>
+            <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+                <Search placeholder="Search invoices..." />
+                <CreateInvoice />
+            </div>
+                {/* table here */}
+            <div className="mt-5 flex w-full justify-center">
+                {/* pagination here */}
+            </div>
+        </div>
+    )
 }
